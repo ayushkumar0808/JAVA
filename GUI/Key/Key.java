96b4c41d9256bf7765f63dfceceab4bbb6a04848
Ayush Kumar <kayush3647@gmail.com>
2026-07-22 01:51:07 +0530

    refresh

---
+++ GUI/Key/Key.java
@@ -0,0 +1,96 @@
+import javax.swing.*; 
+
+import java.awt.event.*; 
+import java.awt.*;
+
+ 
+
+public class Key extends JFrame implements KeyListener  
+
+{ 
+
+ 
+
+    JLabel l; 
+
+    JTextField t; 
+
+ 
+
+    Key()  
+
+   { 
+
+        l = new JLabel("Press any key..."); 
+
+ 
+
+        t = new JTextField(); 
+
+ 
+
+        add(t); 
+
+        add(l); 
+
+ 
+
+        t.addKeyListener(this); 
+
+ 
+
+        setTitle("Keyboard Event Demo"); 
+
+        setSize(400, 250); 
+
+        setLayout(null); 
+
+        setVisible(true); 
+
+    } 
+
+ 
+
+    public void keyTyped(KeyEvent e)  
+
+    { 
+
+        l.setText("Key Typed: " + e.getKeyChar()); 
+
+    } 
+
+ 
+
+    public void keyPressed(KeyEvent e) 
+
+    { 
+
+        l.setText("Key Pressed: " + e.getKeyChar()); 
+
+    } 
+
+ 
+
+    public void keyReleased(KeyEvent e) 
+
+    { 
+
+        l.setText("Key Released: " + e.getKeyChar()); 
+
+    } 
+
+ 
+
+    public static void main(String[] args) 
+
+    { 
+
+        new Key(); 
+
+    } 
+
+} 
+
+ 
+
+ 
